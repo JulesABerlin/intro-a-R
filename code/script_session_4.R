c098209552531a394120465b2cca43cc34e81398
Julian Maitra <JulesABerlin@users.noreply.github.com>
2024-03-14 08:47:13 +0100

    iris wikimedia

--- code/script_session_4.R
+++ code/script_session_4.R
@@ -15,3 +15,52 @@ library(ggthemes)
 
 setwd("/Users/domus_julian/Documents/GitHub/intro-a-R/code")
 
+# jeux de données intégrés
+
+data(iris)
+
+dim(iris)
+
+str(iris)
+
+
+data(mtcars)
+
+?mtcars
+
+str(mtcars)
+
+mean(mtcars$mpg)
+
+
+glimpse(mtcars)
+
+
+data(diamonds)
+
+?diamonds
+
+dim(diamonds)
+
+names(diamonds)
+
+# ggplot avec l'exemple diamonds
+
+library(ggthemes)
+
+ggplot(data = diamonds,
+       mapping = aes(x = carat, 
+                     y = price,
+                     color = clarity)) +
+  geom_point() +
+  labs(title = "Relation entre carat et prix des diamants",
+       subtitle = "n = 53 940 diamants",
+       y = "Prix",
+       x = "Carat") +
+  theme_economist()
+
+
+
+
+
+

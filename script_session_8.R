ed77c164e26e4b61c572521dbae8cacdb3a0677f
Julian Maitra <JulesABerlin@users.noreply.github.com>
2024-04-17 17:00:12 +0200

    session 8 v4

---
+++ script_session_8.R
@@ -0,0 +1,47 @@
+# Script session 8
+
+library(tidyverse)
+
+# setwd(...) (à compléter)
+
+# Problème 1
+
+
+
+# Problème 2
+
+# Problème 3
+
+# Problème 4
+
+# Problème 5
+
+# Problème 6
+
+# Problème 7
+
+# Problème 8
+
+# Problème 9
+
+# Problème 10
+
+# Problème 11
+
+# Problème 12
+
+# Problème 13
+
+# Problème 14
+
+# Problème 15
+
+# Problème 16
+
+# Problème 17
+
+# Problème 18
+
+# Problème 19
+
+# Problème 20
